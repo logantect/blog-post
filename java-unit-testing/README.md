38c2a5feab2ae6f5ede53fcf0780f563db4fcd94
eomjeongjae <jeongjae.eom@gmail.com>
2022-03-14 17:55:10 +0900

    Add Java Unit Test Module

---
+++ java-unit-testing/README.md
@@ -0,0 +1,7 @@
+# Unit Testing In Java
+Junit 학습 및 Java API 등을 단위테스트 하기 위한 공간입니다.
+
+
+# References
+- https://junit.org/junit5/docs/current/user-guide/
+- https://www.baeldung.com/java-unit-testing-best-practices
--- spring-transactions/src/test/java/io/jeongjaeeom/spring/service/PostServiceTest.java
+++ spring-transactions/src/test/java/io/jeongjaeeom/spring/service/PostServiceTest.java
@@ -7,12 +7,15 @@ import io.jeongjaeeom.spring.service.PostCommand.RegisterPost;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class PostServiceTest {
 
