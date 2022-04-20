1601cb4d96b242dd444fc00735c19100ebfccbd3
jeongjaeeom <jeongjae.eom@gmail.com>
2022-04-20 11:08:15 +0900

    트랜잭션 전파 및 내부 메소드 호출시 트랙잭션 적용 안됨 테스트.

--- spring-transactions/src/main/java/io/jeongjaeeom/spring/service/PostService.java
+++ spring-transactions/src/main/java/io/jeongjaeeom/spring/service/PostService.java
@@ -2,6 +2,7 @@ package io.jeongjaeeom.spring.service;
 
 import io.jeongjaeeom.spring.domain.Post;
 import io.jeongjaeeom.spring.domain.PostRepository;
+import io.jeongjaeeom.spring.service.PostCommand.UpdatePost;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
 
   private final PostRepository postRepository;
+  private final CommentService commentService;
 
-  public void proxyPost(PostCommand.RegisterPost command) {
-    registerPost(command);
+  @Transactional(readOnly = true)
+  public Long registerPost(PostCommand.RegisterPost command) {
+    Post savedPost = postRepository.save(command.toEntity());
+    return savedPost.getId();
+  }
+
+  public Long registerPostWithComment(PostCommand.RegisterPost command) {
+    Post savedPost = postRepository.save(command.toEntity());
+    commentService.register("sdfrsdf");
+    return savedPost.getId();
+  }
+
+  public void updateProxy(final Long postId, PostCommand.UpdatePost command) {
+    updatePost(postId, command);
   }
 
   @Transactional
-  void registerPost(PostCommand.RegisterPost command) {
-    Post savedPost = postRepository.save(command.toEntity());
-    savedPost.changeContent("제목수정");  // Dirty Checking 작동 안함.
+  void updatePost(final Long postId, UpdatePost command) {
+    Post post = postRepository.findById(postId).orElseThrow();
+    post.changeContent(command.getContent()); // Not Dirty Checking
   }
+
 
 }
